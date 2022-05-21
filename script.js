d2dc30eaa8abdf45e734289f09af4a145e6463e1
Patalin <42389395+Patalin@users.noreply.github.com>
2022-05-21 19:56:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.btn')
+const form   = document.querySelector('.form')
+
+button.addEventListener('click', function() {
+   form.classList.add('form--no') 
+});
